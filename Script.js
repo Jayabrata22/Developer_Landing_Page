599061458e6be1c07bcbdc304f980569917dda3d
[Jayabrata Das] <[dhyanabratad22@gmail.com]>
2024-04-17 11:06:01 +0530

    try 1

--- Script.js
+++ Script.js
@@ -4,11 +4,13 @@ const itemWidth = item.offsetWidth;
 const containerWidth = document.querySelector('.scrollable-container').offsetWidth;
 
 function handleClick(direction) {
-    if (direction === "previous") {
+    console.log('previous')
+    if (direction === 'previous') {
         // Calculate the distance to scroll back
         const scrollDistance = Math.max(0, list.scrollLeft - containerWidth);
         list.scrollTo({ left: scrollDistance, behavior: "smooth" });
     } else {
+        console.log('next')
         // Calculate the distance to scroll forward
         const scrollDistance = Math.min(list.scrollLeft + containerWidth, list.scrollWidth - containerWidth);
         list.scrollTo({ left: scrollDistance, behavior: "smooth" });
